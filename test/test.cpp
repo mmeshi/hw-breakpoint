5218236b74d88fee0d631429b182145586170968
mmeshi <meir.meshi@gmail.com>
2015-01-26 17:20:08 +0200

    buitify test outpus with colors

--- test/test.cpp
+++ test/test.cpp
@@ -2,21 +2,36 @@
 #include <iostream>
 #include "..\breakpoint.h"
 
+// extending console output with colors
+enum ConsoleColor
+{
+	White = 7,
+	Green = 10,
+	Red = 12
+};
+std::ostream& operator<<(std::ostream& os, ConsoleColor color)
+{
+	static HANDLE hConsole = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	::SetConsoleTextAttribute(hConsole, color);
+	return os;
+};
+
 // globals
 int g_val = 0;
 HANDLE g_hEvent1, g_hEvent2;
 
 void tryWrite()
 {
-	std::cout << "thread " << std::hex << ::GetCurrentThreadId() << " trying to write...";
+	std::cout << White << "thread " << std::hex << ::GetCurrentThreadId() << " trying to write...";
 
 	__try 
     	{
 		g_val = 1;
+		std::cout << "\tmissed writes " << Red << "[failed]" << White << "\n\n" << std::flush;
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER)
 	{
-		std::cout << "\tcatch write attempt [ok]" << std::endl << std::endl;
+		std::cout << "\tcatch write attempt " << Green << "[ok]" << White << "\n\n" << std::flush;
 	}
 }
 
@@ -43,8 +58,8 @@ int main()
 	g_hEvent2 = ::CreateEvent(NULL, TRUE, FALSE, NULL);
 
     	// multi-thread testing:
-	std::cout << std::endl << std::endl << "test 1: existing thread before the BP has setting" << std::endl;
-	std::cout << "=============================================================================" << std::endl;
+	std::cout << "\n\ntest 1: existing thread before the BP has setting";
+	std::cout <<   "\n=================================================" << std::endl;
 	hTrd = ::CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadWriteFunc, NULL, 0, &threadId);
 	
     	// wait for new thread creation
@@ -67,8 +82,8 @@ int main()
 	::ResetEvent(g_hEvent1);
 	::ResetEvent(g_hEvent2);
 
-	std::cout << std::endl << std::endl << "test 2: new thread after setting the BP" << std::endl;
-	std::cout << "=============================================================================" << std::endl;
+	std::cout << "\n\ntest 2: new thread after setting the BP";
+	std::cout <<   "\n=======================================" << std::endl;
 	hTrd = ::CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadWriteFunc, NULL, 0, &threadId);
 
     	// wait for new thread creation
